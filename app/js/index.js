2ca175220849e13527bca42943223d7acd9e81a1
Biloholovskyi <biloholovskyi>
2021-10-28 20:02:15 +0300

    responsive header

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,18 @@
 import 'normalize.css';
 
 $(document).ready((e) => {
+  // show mobile menu
+  $('.header__humb').on('click', function () {
+    const headerNav = $('.header .navigations');
+    headerNav.toggleClass('active');
 
+    if(headerNav.hasClass('active')) {
+      $(this).children('img').attr('src', '../media/icon/close.svg')
+    } else {
+      $(this).children('img').attr('src', '../media/icon/humb.svg')
+    }
+
+  })
 });
 
 $(window).resize(() => {
