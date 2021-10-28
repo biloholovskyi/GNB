5b06f148235bda01c6c0184d2dc7f939fd6b3c19
Biloholovskyi <biloholovskyi>
2021-10-29 00:16:36 +0300

    cases responsive

--- app/js/index.js
+++ app/js/index.js
@@ -20,13 +20,13 @@ $(document).ready((e) => {
   })
 
   // show case
-  $('.cases__item .case-text .more-btn').on('click', cases.showCases)
+  $('.cases__item').on('click', cases.showCases)
   // close case
   $('.cases .cases__more .close').on('click', cases.closeCases)
 
   // close case modal after click on body
   $(document).on('click', function (e) {
-    const caseModal = $('.cases .cases__more .body, .cases__item .more-btn');
+    const caseModal = $('.cases .cases__more .body, .cases__item');
     if (!caseModal.is(e.target) && caseModal.has(e.target).length === 0) {
       cases.closeCases();
     }
